c6924d7c4bcd9b14abfcebca201daaa76dd71a57
Kyle Zielinski <kizielinski219@gmail.com>
2019-02-17 07:25:19 -0500

    clear commit

--- src/Game/GUI.java
+++ src/Game/GUI.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -30,8 +31,14 @@ public class GUI extends Application {
      * Fields
      */
     Button button;
-    int x;
-    int y;
+    DropShadow shadow;
+    int gap;
+    int columns;
+
+    public GUI(int gap, int columns) {
+        this.gap = gap;
+        this.columns = columns;
+    }
 
     public static void main(String[] args) {
         launch(args);
@@ -41,9 +48,7 @@ public class GUI extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Title of the Window");
 
-        DropShadow shadow = new DropShadow();
-        x = 20;
-        y = 20;
+        shadow = new DropShadow();
 
         /**
          * Inputs files and sets them to an image
@@ -58,30 +63,31 @@ public class GUI extends Application {
         /**
          * Sets the box and line to imageview objects
          */
-        ImageView box2 = new ImageView(box);
-        ImageView line2 = new ImageView(line);
+   +    //ImageView box2 = new ImageView(box);
+        //ImageView line2 = new ImageView(line);
 
         /**
          * Sets color for grid background
+         *
          */
-        Color redColor = Color.rgb( 255, 0 , 0, 0.5);
+        Color redColor = Color.rgb(255, 0, 0, 0.5);
         BackgroundFill backgroundFill = new BackgroundFill(redColor, null, null);
-        Background background= new Background(backgroundFill);
+        Background background = new Background(backgroundFill);
 
+        Label label = new Label();
         /**
          * Makes Tilepane:Grid and sets its values.
          * Make sure that the Pref columns, # of dots, and distance between them are related.
          */
         TilePane grid = new TilePane();
-        grid.setPrefColumns(8);
+        grid.setPrefColumns(4);
         grid.setBackground(background);
         grid.resize(490, 540);
 
         /**
          * Adds hover effect to the dots.
          */
-        for(int x = 0; x < 64; x++)
-        {
+        for (int x = 0; x < 16; x++) {
             ImageView imageView = new ImageView(image);
             imageView.addEventHandler(MouseEvent.MOUSE_ENTERED,
                     e -> imageView.setEffect(shadow));
@@ -96,22 +102,24 @@ public class GUI extends Application {
          * the dots both vertically
          * and horizontally.
          */
-        grid.setHgap(32);
-        grid.setVgap(32);
+        grid.setHgap(8);
+        grid.setVgap(8);
         Pane pane = new Pane();
         //pane.setBackground(background);
         pane.resize(490, 540);
-
+        //grid.setAlignment(grid, pane.getMaxWidth()/2 );
         /**
          * Adds everything to the pane and relocates as necessary.
          */
         pane.getChildren().add(grid);
         Group root = new Group(box2);
+        Group root2 = new Group(line2);
         pane.getChildren().add(root);
+        pane.getChildren().add(root2);
+        root2.relocate(50, 50);
         //root.relocate(23, 22);
         //x diff 64
         //y diff 64
-
         root.relocate(23, 86);
 
         //StackPane layout = new StackPane();
@@ -135,7 +143,7 @@ public class GUI extends Application {
         //primaryStage.show();
 
 
-
     }
-
 }
+
+
--- src/Game/Game.java
+++ src/Game/Game.java
@@ -18,16 +18,13 @@ public class Game implements Paint {
     public void Intialize()
     {
         dots = new Board();
-        dots = new Board(10);
+
     }
     public static void main(String[] args) {
         System.out.println("Hello, world");
     }
 
-    @Override
-    public PaintContext createContext(ColorModel cm, Rectangle deviceBounds, Rectangle2D userBounds, AffineTransform xform, RenderingHints hints) {
-        return null;
-    }
+
 
     @Override
     public int getTransparency() {
